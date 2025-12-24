ed898a2577ce58216075df390e319aecc6e0fdb1
KuKuzmiy <kuzma.dobrovolsky@gmail.com>
2025-12-24 17:32:50 +0100

    added Task1_5

--- cypress/e2e/Task1_5.cy.js
+++ cypress/e2e/Task1_5.cy.js
@@ -1,5 +1,22 @@
 import { faker } from '@faker-js/faker'
+import { RegisterPage } from '../pages/RegisterPage'
 
-  it('Register new user', () => {
-    cy.visit('https://juice-shop.herokuapp.com/#/contact')
-  })
+const page = new RegisterPage()
+
+it('fills feedback comment', () => {
+  page.visitFeedback()
+  page.dismissWelcomeBanner()
+  page.acceptCookies()
+
+  const comment = faker.lorem.sentence(10).slice(0, 150)
+
+  page.typeFeedbackComment(comment)
+  page.assertFeedbackCommentValue(comment)
+
+  page.setRating(5)
+
+  page.solveCaptchaAndTypeResult()
+
+  page.submitFeedback()
+
+})
--- cypress/pages/RegisterPage.js
+++ cypress/pages/RegisterPage.js
@@ -3,6 +3,10 @@ export class RegisterPage {
     cy.visit('https://juice-shop.herokuapp.com/#/register')
   }
 
+  visitFeedback() {
+    cy.visit('https://juice-shop.herokuapp.com/#/contact')
+  }
+
   dismissWelcomeBanner() {
     cy.log('UI: Dismiss welcome banner')
     cy.contains('button', 'Dismiss', { timeout: 10000 }).click()
@@ -43,6 +47,19 @@ export class RegisterPage {
     cy.getById('securityAnswerControl').type(answer)
   }
 
+  typeFeedbackComment(comment) {
+    cy.log('UI: Fill feedback comment')
+    cy.getById('comment')
+      .should('be.visible')
+      .clear()
+      .type(comment)
+  }
+
+  assertFeedbackCommentValue(comment) {
+    cy.log('Assert: Feedback comment value')
+    cy.getById('comment').should('have.value', comment)
+  }
+
   submit() {
     cy.log('UI: Submit registration')
     cy.getById('registerButton')
@@ -56,4 +73,65 @@ export class RegisterPage {
       .should('be.visible')
       .and('contain.text', 'Registration completed successfully')
   }
+
+setRating(value = 5) {
+  cy.log(`UI: Set rating to ${value} stars`)
+
+  cy.get('#rating input[matSliderThumb]', { timeout: 10000 })
+    .should('exist')
+    .then(($el) => {
+      const el = $el[0]
+      el.value = String(value)
+      el.dispatchEvent(new Event('input', { bubbles: true }))
+      el.dispatchEvent(new Event('change', { bubbles: true }))
+    })
+}
+
+getCaptchaExpression() {
+  cy.log('UI: Read captcha expression')
+  return cy.getById('captcha')
+    .should('exist')
+    .invoke('text')
+    .then((t) => t.trim())
+}
+
+calculateExpression(expression) {
+  const sanitized = expression.replace(/\s+/g, '')
+
+  if (!/^[0-9+\-*/().]+$/.test(sanitized)) {
+    throw new Error(`Unsupported captcha expression: "${expression}"`)
+  }
+
+  const result = Function(`"use strict"; return (${sanitized});`)()
+
+  return Number.isFinite(result) ? result : NaN
+}
+
+solveCaptchaAndTypeResult() {
+  cy.log('UI: Solve captcha and type result')
+
+  this.getCaptchaExpression().then((expr) => {
+    const result = this.calculateExpression(expr)
+
+    if (!Number.isFinite(result)) {
+      throw new Error(`Captcha result is not a number. Expr="${expr}", result="${result}"`)
+    }
+
+    cy.log(`CAPTCHA: ${expr} = ${result}`)
+
+    cy.getById('captchaControl')
+      .should('exist')
+      .clear()
+      .type(String(result))
+  })
+}
+
+submitFeedback() {
+  cy.log('UI: Submit feedback')
+  cy.getById('submitButton')
+    .should('be.enabled')
+    .click()
+}
+
+
 }
