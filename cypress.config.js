8876a9556797baa166db1237ddca01a435a8c9ef
KuKuzmiy <kuzma.dobrovolsky@gmail.com>
2025-12-25 02:53:58 +0200

    Updated width and height in Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://juice-shop.herokuapp.com/#/',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
